4dac8d7647e639c3589422e7b1aef4c5556bb143
Сергей Гурьянов <gurjanov.sergey@icloud.com>
2024-10-15 13:15:49 +0300

    g

--- ex2/after_refactoring_sguryanov.cpp
+++ ex2/after_refactoring_sguryanov.cpp
@@ -1,35 +1,30 @@
 #include <cmath>
 #include <iostream>
 
-float average_value (int *arr, int arr_size);
-void generate_and_print_array (int *arr, int arr_size);
+float average_value (int *arr,const int &arr_size);
+void generate_array (int *arr,const int &arr_size);
+void print_array (int *arr,const int &arr_size);
 
 
 int main()
-{   
+{   std::cout << "Введите количество запросов : ";
+    int count_request;
+    std::cin >> count_request;
+    std::cout << std::endl;
+
     const int arr_size = 10;
     int arr[arr_size];
     
-    std::cout << "Первая последовательность :" << std::endl;
-    generate_and_print_array(arr,arr_size);
-    
-    std::cout << "Среднее первой последовательности : " << average_value(arr,arr_size) << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "Вторая последовательность :" << std::endl;
-    generate_and_print_array(arr,arr_size);
-
-    std::cout << "Среднее второй последовательности : " << average_value(arr,arr_size) << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "Третья последовательность :" << std::endl;
-    generate_and_print_array(arr,arr_size);
-
-    std::cout << "Среднее третьей последовательности : " << average_value(arr,arr_size) << std::endl;
-    
+    for (int i = 1; i <= count_request; i++){
+        std::cout << "Последовательность №"<< i << std::endl;
+        generate_array(arr,arr_size);
+        print_array(arr,arr_size);
+        std::cout << "Среднее последовательности №"<< i <<" : " << average_value(arr,arr_size) << std::endl;
+        std::cout << std::endl;
+    }
 }
 
-float average_value (int *arr, int arr_size){
+float average_value (int *arr, const int &arr_size){
     float sum = 0;
     for(int i = 0; i < arr_size; i++) {
         sum += arr[i];
@@ -37,10 +32,15 @@ float average_value (int *arr, int arr_size){
     return sum/arr_size;
 }
 
-void generate_and_print_array (int *arr, int arr_size) {
-    const char format = ' ';
+void generate_array (int *arr,const int &arr_size) {
     for(int i = 0; i < arr_size; i++) {
         arr[i] = random() % 20;
+    }
+}
+
+void print_array (int *arr,const int &arr_size) {
+    const char format = ' ';
+    for(int i = 0; i < arr_size; i++) {
         std::cout << arr[i] << format;
     }
     std::cout<<std::endl;
